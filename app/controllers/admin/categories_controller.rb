d3f70c4189550ed01efea639cac139e0d08ce1ac
Bazil <king.bazil.khan@gmail.com>
2022-07-07 00:22:15 +0000

    Adds categories controller for admin use

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,25 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @category = Category.order(:name).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to %i[admin categories], notice: "Category created! ✅"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
